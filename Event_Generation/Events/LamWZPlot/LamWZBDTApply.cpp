9a0d510104528e1b89577545effe1acb258711df
Yongcheng Wu <yc_wu1030@126.com>
2019-08-13 19:11:26 -0400

    Add BDT Test
    Apply the BDT getting from previous training to the events.

---
+++ Event_Generation/Events/LamWZPlot/LamWZBDTApply.cpp
@@ -0,0 +1,142 @@
+#include "plotheader.h"
+#include "LamWZPreAna.h"
+#include "TStyle.h"
+#include "TH2F.h"
+#include "Utilities.h"
+#include "CutList.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "TMVA/Factory.h"
+#include "TMVA/DataLoader.h"
+#include "TMVA/Tools.h"
+#include "TMVA/TMVAGui.h"
+#include "TMVA/Config.h"
+#include "TMVA/Reader.h"
+
+using namespace std;
+string DIR = "/data/data068/ycwu/LamWZ/Event_Generation/Events/Plots";
+const int LUMINOSITY = 4000; //fb^-1
+const int N_SIGCAT = 3;
+const int N_BKGCAT = 3;
+const int N_VARIABLE = 30;
+string DecayChannel[2] = {"bbll","tatall"};
+string SIG_NAME[N_SIGCAT] = {"wh_hWW","wh_hZZ","wh_Inter"};
+string BKG_NAME[N_BKGCAT] = {"tt","wz","zz"};
+string SIG_LABEL[N_SIGCAT] = {"Wh hWW","Wh hZZ","Wh Interference"};
+string BKG_LABEL[N_BKGCAT] = {"t#bar{t}","WZ","ZZ"};
+int Sig_NTOTAL[N_SIGCAT] = {247244,247138,247328};
+int Bkg_NTOTAL[N_BKGCAT] = {2500000,2500000,2500000};
+
+int main(int argc, char const *argv[])
+{
+    SetPlotStyle();
+
+    if (argc != 4) return -1;
+    string InputDir(argv[1]);
+    int decayID=atoi(argv[2]);
+    string tag(argv[3]);
+
+    //Create the Folder Saving the results
+    string dir,dirtop;
+    stringstream sst,ssttop;
+    ssttop<<DIR;
+    ssttop>>dirtop;
+    createdir(dirtop.c_str());
+    sst<<dirtop<<"/BDT_Train_LamWZ_"<<tag;
+    sst>>dir;
+    createdir(dir.c_str());
+
+
+    char temp[500];
+    TChain *Chaintotal = new TChain("LamWZPreAna");
+    for (int i = 0; i < N_SIGCAT; ++i)
+    {
+        sprintf(temp,"%s/%s*.root",InputDir.c_str(),SIG_NAME[i].c_str());
+        Chaintotal -> Add(temp);
+        // LamWZPreAna *ch = new LamWZPreAna(ChainSIG[i]);
+        // ch->GetEntry(0);
+        // WeightSIG[i] = ch->CS*LUMINOSITY/((double)Sig_NTOTAL[i]);
+    }
+    for (int i = 0; i < N_BKGCAT; ++i)
+    {
+        sprintf(temp,"%s/%s*.root",InputDir.c_str(),BKG_NAME[i].c_str());
+        Chaintotal -> Add(temp);
+        // LamWZPreAna *ch = new LamWZPreAna(ChainBKG[i]);
+        // ch->GetEntry(0);
+        // WeightBKG[i] = ch->CS*LUMINOSITY/((double)Bkg_NTOTAL[i]);
+    }
+    LamWZPreAna *ch = new LamWZPreAna(Chaintotal);
+
+    TMVA::Tools::Instance();
+    sprintf(temp,"%s/BDTResult.root",dir.c_str());
+    TFile *f2 = new TFile(temp,"RECREATE");
+    TTree *t2 = new TTree("BDTResult","The results from BDT");
+    double BDTScore;
+    int cat;
+    int SorB;
+    double Weight;
+
+    t2->Branch("Cate",&cat,"Cate/I");
+    t2->Branch("SorB",&SorB,"SorB/I");
+    t2->Branch("Weight",&Weight,"Weight/D");
+    t2->Branch("BDTScore",&BDTScore,"BDTScore/D");
+
+    TMVA::Reader *reader = new TMVA::Reader("!Color:Silent");
+
+    Float_t F_HT, F_Mbb, F_Mll, F_AnglebV;
+    reader->AddVariable( "HT", &F_HT );
+    reader->AddVariable( "Mbb", &F_Mbb );
+    reader->AddVariable( "Mll", &F_Mll );
+    reader->AddVariable( "AnglebV", &F_AnglebV );
+
+    
+    TCut precuts = "NBJet==2&&NLep_Af==2";
+    string methodName = "BDT method";
+    string weightfile = dir + "/TMVAClassification_BDT.weights.xml";
+    reader->BookMVA(methodName,weightfile);
+
+    // --------------------------------------------------------------
+    Int_t nentries = Int_t(Chaintotal->GetEntries());
+    Int_t Good=true;
+    // int SorB; // 1 for Sig, 0 for Bkg;
+    int id; // Histogram ID;
+    printf("%d entries to be processed!\n",nentries);
+    for (int entry = 0; entry < nentries; ++entry)
+    {
+        if((entry+1)%100000==0) {cout<<entry+1<<" entries processed...\r"; cout.flush();}
+        ch->GetEntry(entry);
+        cat = ch->Cate;
+        Good = (ch->NBJet == 2 && ch->NLep_Af==2);
+        if(!Good) continue;
+        GetHistID(cat,SorB,id);
+        if (SorB == 1)
+        {
+            Weight = ch->CS*LUMINOSITY/((double)Sig_NTOTAL[id]);
+        }
+        else
+        {
+            Weight = ch->CS*LUMINOSITY/((double)Bkg_NTOTAL[id]);
+        }
+        F_HT = ch->HT;
+        F_Mll = ch->Mll;
+        F_Mbb = ch->Mbb;
+        F_AnglebV = ch->AnglebV;
+        BDTScore = reader->EvaluateMVA("BDT method");
+        t2->Fill();
+    }
+
+
+    f2->cd();
+    t2->Write();
+
+    delete t2;
+    f2->Close();
+    cout<<"Program Exit!"<<endl;
+    cout<<"=================================>"<<endl;
+    return 0;
+}
