6e6a8c25b1446869754a18aa19aa434923b3b80e
Yongcheng Wu <yc_wu1030@126.com>
2020-04-26 01:26:01 -0400

    Update processes syntax Remove un-used process Update run card to fit to CLIC Add calculate CS function to MGrunning

--- Event_Generation/MGrunningJSON.py
+++ Event_Generation/MGrunningJSON.py
@@ -32,22 +32,26 @@ if not os.path.exists(Event_dir):
 
 parser = argparse.ArgumentParser(prog='MGrunning',description='Running MadGraph for process generation and/or event generation.')
 parser.add_argument('-g',dest='flag_g',action='store_true') #Generate the MG5 directories
+parser.add_argument('-cs',dest='flag_cs',action='store_true') # Calculate the CS for hard processes 
 parser.add_argument('-r',dest='flag_r',action='store_true') #Running the MG5
 parser.add_argument('-delphes',dest='flag_delphes',action='store_true')
 parser.add_argument('-channel',dest='decays',default='bbll')
 parser.add_argument('-i',dest='processfile',default='processes_list.json')
 parser.add_argument('-s',dest='selection',type=simplejson.loads,default='{}')
 parser.add_argument('-e',dest='sqrts',default=3000,type=int) # in GeV
+parser.add_argument('-o',dest='cs_output',default='processes_hard_cs_list.json')
 args = parser.parse_args()
 #print(args)
 
 flag_g = args.flag_g
+flag_cs = args.flag_cs
 flag_r = args.flag_r
 flag_delphes = args.flag_delphes
 decays = args.decays
 processfile = args.processfile
 selection = args.selection
 sqrts = args.sqrts
+cs_output = args.cs_output
 MATCH=True
 
 def SELECT_PROCESS(Process,SELECTS):
@@ -83,6 +87,55 @@ if flag_g:
         subprocess.call(mg5Dir + '/bin/mg5_aMC tmp_mg5.dat', shell=True)
         os.remove('tmp_mg5.dat')
 
+if flag_cs:
+    cs_results = []
+    for i in range(len(ProcessesList)):
+        if not SELECT_PROCESS(ProcessesList[i],selection):
+            print('Skip: ',ProcessesList[i]['Name'])
+            continue
+        with open('tmp_mg5.dat','w') as OutGen:
+            OutGen.write('import model ' + modelName + '\n')
+            ChannelProcesses=ProcessesList[i]['HardProcesses']
+            OutGen.write('generate ' + ChannelProcesses[0] + '\n')
+            for j in range(1,len(ChannelProcesses)):
+                OutGen.write('add process ' + ChannelProcesses[j] + '\n')
+            OutGen.write('output ' + work_dir + '/' + ProcessesList[i]['Name'] + '_Hard_CS' + ' -f \n')
+            OutGen.write('exit\n')
+        subprocess.call(mg5Dir + '/bin/mg5_aMC tmp_mg5.dat', shell=True)
+        os.remove('tmp_mg5.dat')
+        tag=time.strftime("%m%d_%H")
+        Process=ProcessesList[i]
+        with open('tmp_madevent.dat','w') as MECOM:
+            MECOM.write('generate_events run_' + '%d'%(sqrts) + "_" + tag + ' -f \n')
+        if sqrts == 3000:
+            shutil.copyfile(work_dir + '/Cards_tmp/run_card.dat', work_dir + '/' + Process['Name'] + '_Hard_CS' + '/Cards/run_card.dat')
+        elif sqrts == 1500:
+            shutil.copyfile(work_dir + '/Cards_tmp/run_card_1500.dat', work_dir + '/' + Process['Name'] + '_Hard_CS' + '/Cards/run_card.dat')
+        else:
+            shutil.copyfile(work_dir + '/Cards_tmp/run_card.dat', work_dir + '/' + Process['Name'] + '_Hard_CS' + '/Cards/run_card.dat')
+        shutil.copyfile(work_dir + '/Cards_tmp/param_card.dat', work_dir + '/' + Process['Name'] + '_Hard_CS' + '/Cards/param_card.dat')
+        shutil.copyfile(work_dir + '/Cards_tmp/me5_configuration.txt', work_dir + '/' + Process['Name'] + '_Hard_CS' + '/Cards/me5_configuration.txt')
+        gene_dir = work_dir + '/' + Process['Name'] + '_Hard_CS' + '/Events/run_' + '%d'%(sqrts) + "_" + tag
+        try:
+            os.remove(work_dir + '/' + Process['Name'] + '_Hard_CS' + '/Cards/madspin_card.dat')
+        except OSError:
+            pass
+        try:
+            os.remove(work_dir + '/' + Process['Name'] + '/Cards/delphes_card.dat')
+        except OSError:
+            pass
+        try:
+            os.remove(work_dir + '/' + Process['Name'] + '/Cards/pythia8_card.dat')
+        except OSError:
+            pass
+        subprocess.call(work_dir + '/' + Process['Name'] + '/bin/madevent ' + currentDir + '/tmp_madevent.dat', cwd = work_dir + '/' + Process['Name'], shell=True)
+        gene_dir = work_dir + '/' + Process['Name'] + '/Events/run_' + '%d'%(sqrts) + "_" + tag
+        result_file = gene_dir + '/run_' + '%d'%(sqrts) + "_" + tag + '_tag_1_banner.txt'
+        cs=subprocess.check_output('awk \'$2=="Integrated"&&$3=="weight" {print $6}\' ' + result_file,shell=True)
+        cs_results.append({Process['Name']: float(cs)*1000})
+    with open(cs_output,'w') as cs_tmp:
+        simplejson.dump(cs_results,cs_tmp)
+
 if flag_r:
     for i in range(len(ProcessesList)):
         tag=time.strftime("%m%d_%H")
@@ -106,7 +159,7 @@ if flag_r:
         if Process['Decay'] == 'None':
             Process['Decay'] = decays
         with open('tmp_madevent.dat','w') as MECOM:
-            if Process['Abbr'] == 'zh' and Process['BkgSigTag'] != 'bkg':
+            if Process['Abbr'] == 'zh' and Process['BkgSigTag'] != 'Full':
                 MECOM.write('generate_events run_' + '%d'%(sqrts) + "_" + tag + '\n')
                 MECOM.write('done\n')
                 MECOM.write('set mmll 80.0\n')
@@ -123,7 +176,7 @@ if flag_r:
         shutil.copyfile(work_dir + '/Cards_tmp/param_card.dat', work_dir + '/' + Process['Name'] + '/Cards/param_card.dat')
         shutil.copyfile(work_dir + '/Cards_tmp/me5_configuration.txt', work_dir + '/' + Process['Name'] + '/Cards/me5_configuration.txt')
 	#print Process['Name'],Process['BkgSigTag']
-        if Process['BkgSigTag'] != 'bkg':
+        if Process['BkgSigTag'] != 'bkg' and Process['BkgSigTag'] != 'Full':
             #print 'Signal'
             gene_dir = work_dir + '/' + Process['Name'] + '/Events/run_' + '%d'%(sqrts) + "_" + tag + "_decayed_1"
             shutil.copyfile(work_dir + '/Cards_tmp/madspin_card_' + decays + '.dat', work_dir + '/' + Process['Name'] + '/Cards/madspin_card.dat')
