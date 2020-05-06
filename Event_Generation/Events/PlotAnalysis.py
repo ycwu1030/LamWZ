75725d4200d02023f95fa8f81f1756132ecaaa15
Yongcheng Wu <yc_wu1030@126.com>
2020-05-06 17:07:31 -0400

    Adding quote

--- Event_Generation/Events/PlotAnalysis.py
+++ Event_Generation/Events/PlotAnalysis.py
@@ -70,11 +70,11 @@ with open(ProcessesFile,'r') as f:
             nsig+=1
 
 
-signame_str=','.join(signame)
-bkgname_str=','.join(bkgname)
-siglabel_str=','.join(siglabel)
-bkglabel_str=','.join(bkglabel)
+signame_str='\",\"'.join(signame)
+bkgname_str='\",\"'.join(bkgname)
+siglabel_str='\",\"'.join(siglabel)
+bkglabel_str='\",\"'.join(bkglabel)
 signeve_str=','.join(signeve)
 bkgneve_str=','.join(bkgneve)
 
-subprocess.call("sed -e 's/__LUMI__/%d/g' -e 's/__NSIG__/%d/g' -e 's/__NBKG__/%d/g' -e 's/__SIGNAME__/%s/g' -e 's/__BKGNAME__/%s/g' -e 's/__SIGLABEL__/%s/g' -e 's/__BKGLABEL__/%s/g' -e 's/__SIGNEVE__/%s/g' -e 's/__BKGNEVE__/%s/g' %s/%s_tmp.cpp > %s/%s.cpp "%(Lumi,nsig,nbkg,signame_str,bkgname_str,siglabel_str,bkglabel_str,signeve_str,bkgneve_str,SRCDIR,PlotsNAME,SRCDIR,PlotsNAME),shell=True)
+subprocess.call("sed -e 's/__LUMI__/%d/g' -e 's/__NSIG__/%d/g' -e 's/__NBKG__/%d/g' -e 's/__SIGNAME__/\"%s\"/g' -e 's/__BKGNAME__/\"%s\"/g' -e 's/__SIGLABEL__/\"%s\"/g' -e 's/__BKGLABEL__/\"%s\"/g' -e 's/__SIGNEVE__/%s/g' -e 's/__BKGNEVE__/%s/g' %s/%s_tmp.cpp > %s/%s.cpp "%(Lumi,nsig,nbkg,signame_str,bkgname_str,siglabel_str,bkglabel_str,signeve_str,bkgneve_str,SRCDIR,PlotsNAME,SRCDIR,PlotsNAME),shell=True)
