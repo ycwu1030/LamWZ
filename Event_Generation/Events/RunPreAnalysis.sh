1839bfe566226f71028098a9f916e1b153b0d3f0
Yongcheng Wu <yc_wu1030@126.com>
2020-05-19 10:58:38 -0400

    Update process list for different lwz analysis

--- Event_Generation/Events/RunPreAnalysis.sh
+++ Event_Generation/Events/RunPreAnalysis.sh
@@ -2,11 +2,12 @@
 
 topdir="/data/data068/ycwu/LamWZ/Event_Generation/Events"
 
-for mode in 'wh' 'zh'
+for mode in 'wh' #'zh'
     do
       for sqrts in 1500 3000
          do
-           python PreAnalysis.py -o $topdir/PreAna_"$mode"_"$sqrts"_SM -i ../processes_list.json -e $sqrts -m $mode
+          #  python PreAnalysis.py -o $topdir/PreAna_"$mode"_"$sqrts"_SM -i ../processes_list.json -e $sqrts -m $mode
+           python PreAnalysis.py -o $topdir/PreAna_"$mode"_"$sqrts"_diffwz -i ../processes_list_difflwz.json -e $sqrts -m $mode
          done
     done
 
